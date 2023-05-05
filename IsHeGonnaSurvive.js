b0826ccf2263e7a4054ee553dcd3ab7dea956bb5
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2023-05-06 00:18:25 +0300

    Add files via upload

---
+++ IsHeGonnaSurvive.js
@@ -0,0 +1,11 @@
+<script>
+
+function hero(bullets, dragons){
+  if (bullets >= (dragons * 2)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+</script>
---
+++ ReturnNegative.js
@@ -0,0 +1,13 @@
+<script>
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else if (num == 0) {
+    return 0;
+  } else if (num < 0) {
+    return num
+  }
+}
+
+</script>
