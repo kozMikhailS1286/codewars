a0a4c3d4bf44dbbe17a970793c03d6c8ab8da3bb
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2024-03-06 17:30:11 +0300

    Add files via upload

---
+++ FundamentaSReturn.js
@@ -0,0 +1,27 @@
+<script>
+
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return ((a % b) + b) % b;
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a-b
+}
+
+</script>
