75d09d27605dffc2006a673c8729512f105a4b13
Mikhail <109968626+kozMikhailS1286@users.noreply.github.com>
2024-04-27 13:28:33 +0300

    Add files via upload

---
+++ FlickSwitch.js
@@ -0,0 +1,11 @@
+<script>
+
+function flickSwitch(arr){
+               let a = true;
+            return arr.map(x => {
+                if (x === "flick") a = !a
+                    return a
+            })
+}
+
+</script>
